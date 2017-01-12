437bd425ded9bb0161fefc874cb66870fdafff83
boblaublaw <github@sublimation.org>
2017-01-11 18:11:23 -0800

    generic user /Users/joe

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-s3cmd --config=/Users/k/.s3LGfullaccess sync public/ s3://lookingglassvr --exclude '.DS_Store'
-s3cmd setacl --config=/Users/k/.s3LGfullaccess s3://lookingglassvr/ --acl-public --recursive
+s3cmd --config=~/.s3LGfullaccess sync public/ s3://lookingglassvr --exclude '.DS_Store'
+s3cmd setacl --config=~/.s3LGfullaccess s3://lookingglassvr/ --acl-public --recursive
