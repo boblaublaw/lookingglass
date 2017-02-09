5cb5d6bb8f8f05b1fcf7dcbf47f76586e4adf1f4
boblaublaw <github@sublimation.org>
2017-02-09 14:49:50 -0800

    fixing silly css content type thing

--- operations/deploy.sh
+++ operations/deploy.sh
@@ -2,3 +2,4 @@
 
 s3cmd --config=${HOME}/.s3LGfullaccess --exclude '.DS_Store' sync public/ s3://lookingglassvr/
 s3cmd setacl --config=${HOME}/.s3LGfullaccess --acl-public --recursive s3://lookingglassvr/
+s3cmd --config=${HOME}/.s3LGfullaccess -m text/css modify s3://lookingglassvr/css/main.css
